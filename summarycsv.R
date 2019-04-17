83b53177d5d485e2b5badf24383cd18dc8365ab1
Phill <phillclarke@gmail.com>
2019-04-17 11:18:17 +0000

    Update to use https
    Also removed the writing of an extra XML and made writing the CSV nicer with write.csv() rather than write.table()

--- summarycsv.R
+++ summarycsv.R
@@ -1,7 +1,7 @@
 # Load the packages we need
 
-require(RCurl)
-require(XML)
+library(RCurl)
+library(XML)
 
 # Our UKHR login credentials. Later we will move these to a separate config file.
 
@@ -10,24 +10,20 @@ ukhrpassword = "yourukhrpassword"
 
 # Construct our URL to retrieve today's obsfucated URL, with the relevant login credentials
 
-currentdayIDURL <- paste("http://www.ukhorseracing.co.uk/members/getsuffix.asp?UID=", ukhrusername, "&PWD=", ukhrpassword, sep="")
+currentdayIDURL <- paste("https://www.ukhorseracing.co.uk/members/getsuffix.asp?UID=", ukhrusername, "&PWD=", ukhrpassword, sep="")
 
 # Download today's obsfucated URL
 
 getcurrentdayID <- getURL(currentdayIDURL) 
 
-# Write the returned XML file to disk
+# Extract just the ID from the XML
 
-write.table(getcurrentdayID, file = "currentdayID.xml", row.names = FALSE, col.names = FALSE, quote = FALSE) 
-
-# Extract just the ID from the XML file
-
-storedpageID = xmlRoot(xmlTreeParse("currentdayID.xml"))
+storedpageID <- xmlRoot(xmlTreeParse(getcurrentdayID))
 
 function(node) 
-	xmlSApply(node, xmlValue)
+  xmlSApply(node, xmlValue)
 
-currentdayID = xmlSApply(storedpageID[[2]], xmlValue)
+currentdayID <- xmlSApply(storedpageID[[2]], xmlValue)
 
 # Find today's date in the relevant format
 
@@ -35,10 +31,14 @@ datetoday <- format(Sys.Date(), "%Y%m%d")
 
 # Construct the full download URL for today's summary CSV
 
-todaysummaryURL <- paste("http://www.ukhorseracing.co.uk/members/ratings/", datetoday, "summary", currentdayID, ".csv", sep="")
+todaysummaryURL <- paste("https://www.ukhorseracing.co.uk/members/ratings/", datetoday, "summary", currentdayID, ".csv", sep="")
 
-# Retrieve the CSV and write it to file
+# Retrieve the summary CSV, which comes as a large character of text
 
 todaysummaryCSV <- getURL(todaysummaryURL)
 
-write.table(todaysummaryCSV, file = "summaryCSV.csv", row.names = FALSE, col.names = FALSE, quote = FALSE) 
+# Convert large character to dataframe and then write to CSV
+
+todaysummary_df <- read.csv(text = todaysummaryCSV)
+write.csv(todaysummary_df, file = "summaryCSV.csv", row.names = FALSE)
+
